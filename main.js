cdc4df711c7930c3a61a05c8bcfcef707510b682
artemave <artemave@gmail.com>
2011-03-26 13:28:02 +0000

    load app after dom loaded

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 require({
-    paths: {cs: 'lib/rjs-cs', text: 'lib/rjs-text-min'},
-    priority: ['lib/jquery', 'lib/underscore-min']
+    paths: { // RequireJS plugin aliases
+      cs: 'lib/rjs-cs',
+      text: 'lib/rjs-text-min'
+    },
+    priority: ['lib/jquery-1.5.1.min', 'lib/underscore-min']
   },
-  ['cs!views/app', 'lib/jquery', 'lib/backbone-min', 'lib/underscore-min'], function(app_view) {
-    window.App = new app_view
+  ['cs!views/app', 'lib/jquery-1.5.1.min', 'lib/backbone-min', 'lib/underscore-min'], function(app_view) {
+    $(function() {
+      window.App = new app_view;
+    });
   }
 );
