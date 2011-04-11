f552b026a1a6bededd465a2c6b48a672e2e6d427
artemave <artemave@gmail.com>
2011-04-11 16:03:33 +0100

    rearrange app structure

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 require({
     paths: { // RequireJS plugin aliases
-      cs: 'vendor/rjs-cs',
-      text: 'vendor/rjs-text-min'
+      cs: '../vendor/rjs-cs',
+      text: '../vendor/rjs-text-min',
+      vendor: '../vendor'
     },
     priority: ['vendor/jquery-1.5.1.min', 'vendor/underscore-min', 'vendor/webtoolkit.base64']
   },
-  ['cs!views/app', 'cs!lib/config', 'vendor/jquery-1.5.1.min', 'vendor/backbone-min', 'vendor/underscore-min', 'vendor/webtoolkit.base64'], function(AppView, AppConfig) {
+  ['cs!views/app', 'cs!config', 'vendor/jquery-1.5.1.min', 'vendor/backbone-min', 'vendor/underscore-min', 'vendor/webtoolkit.base64'], function(AppView, AppConfig) {
     function ignite() {
       window.App = new AppView;
+      //Backbone.history.start();
     };
 
     window.AppConfig = new AppConfig;
