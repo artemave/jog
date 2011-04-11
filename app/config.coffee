4f42f9db37be510cc2fa79084efd4f3e02273cd5
artemave <artemave@gmail.com>
2011-04-11 18:58:14 +0100

    added jasmin

--- app/config.coffee
+++ app/config.coffee
@@ -1,11 +1,15 @@
 define () ->
   class AppConfig
-    constructor: ->
-      this.env = if location.protocol is 'http:' then 'desktop' else 'mobile'
-      this.ljapi_url = ( if this.env is 'mobile' then 'http://www.livejournal.com' else '' ) + '/interface/xmlrpc'
+    constructor: (opts) ->
+      opts = _(opts).defaults
+        env: 'development'
+
+      this.env = opts.env
+      this.current_device = if location.protocol is 'http:' then 'desktop' else 'mobile'
+      this.ljapi_url = ( if this.current_device is 'mobile' then 'http://www.livejournal.com' else '' ) + '/interface/xmlrpc'
 
       # stub out phonegap api to run in firebug
-      if this.env is 'desktop'
+      if this.current_device is 'desktop'
         apis = ['notification']
 
         _(apis).each (api) ->
@@ -14,3 +18,4 @@ define () ->
             # __noSuchMethod__ only works in firefox
             navigator[api].__noSuchMethod__ = (fname, args) ->
               console.log "#{fname} called with [#{args}]"
+
---
+++ app/main.js
@@ -0,0 +1,45 @@
+require({
+    paths: { // RequireJS plugin aliases
+      cs: '../vendor/rjs-cs',
+      text: '../vendor/rjs-text-min',
+      vendor: '../vendor',
+      spec: '../spec'
+    },
+    priority: ['vendor/jquery-1.5.1.min', 'vendor/underscore-min', 'vendor/webtoolkit.base64']
+  },
+  ['cs!views/app', 'cs!config', 'vendor/jquery-1.5.1.min', 'vendor/backbone-min', 'vendor/underscore-min', 'vendor/webtoolkit.base64'], function(AppView, AppConfig) {
+    function ignite_with(fire) {
+      if (window.AppConfig.current_device == 'mobile') {
+        $(document).bind('deviceready', function() { fire() });
+      }
+      else {
+        $(function() { fire() });
+      }
+    };
+
+    window.AppConfig = new AppConfig({env: 'test'});
+
+    if (window.AppConfig.env == 'test') {
+      var specs = [
+        'cs!spec/startup_spec'
+      ];
+      require({
+          priority: ['vendor/jasmine-1.0.2/jasmine', 'vendor/jasmine-1.0.2/jasmine-html']
+        },
+        ['text!vendor/jasmine-1.0.2/jasmine.css', 'vendor/jasmine-1.0.2/jasmine', 'vendor/jasmine-1.0.2/jasmine-html'].concat(specs), function(css) {
+          $('<style type="text/css">').text(css).appendTo('head');
+
+          ignite_with(function() {
+              jasmine.getEnv().addReporter(new jasmine.TrivialReporter());
+              jasmine.getEnv().execute();
+          });
+      })
+    }
+    else {
+      ignite_with(function() {
+          window.App = new AppView;
+          //Backbone.history.start();
+      });
+    }
+  }
+);
