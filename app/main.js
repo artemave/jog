6d7b30d04614244186b9723607a4f058fd411784
artemave <artemave@gmail.com>
2011-05-11 12:51:06 +0100

    Workaround the stunning fact that order!cs! works ONLY in Firefox

--- app/main.js
+++ app/main.js
@@ -1,4 +1,18 @@
 var ENV = 'test';
+var dep_libs = [
+  'vendor/jquery-1.5.1.min',
+  'vendor/underscore-min',
+  'vendor/backbone',
+  'cs!lib/namespace'
+];
+var app_libs = [
+  'cs!config',
+  'cs!controllers/app_controller',
+  'cs!controllers/session',
+  'cs!controllers/friends',
+  'cs!controllers/posts',
+  'cs!models/user'
+];
 
 require({
     paths: {
@@ -8,72 +22,63 @@ require({
       vendor: '../vendor',
       spec: '../spec'
     }
-  },
-  [
-    'order!vendor/jquery-1.5.1.min',
-    'order!vendor/underscore-min',
-    'order!vendor/backbone',
-    'order!cs!lib/namespace',
-    'order!cs!config',
-    'order!cs!controllers/app_controller',
-    'order!cs!controllers/session',
-    'order!cs!controllers/friends',
-    'order!cs!controllers/posts',
-    'order!cs!models/user'
-  ], function() {
-    function ignite_with(fire) {
-      if (Jog.config.current_device == 'mobile') {
-        $(document).bind('deviceready', function() { fire() });
-      }
-      else {
-        $(function() { fire() });
-      }
-    };
+  }, dep_libs, function() {
+      require(app_libs, function() {
+          function ignite_with(fire) {
+            if (Jog.config.current_device == 'mobile') {
+              $(document).bind('deviceready', function() { fire() });
+            }
+            else {
+              $(function() { fire() });
+            }
+          };
 
-    Jog.start = function() {
-      Jog.Controller.main = new Jog.Controller.Main;
-      Jog.Controller.session = new Jog.Controller.Session;
-      Jog.Controller.posts = new Jog.Controller.Posts;
-      Jog.Controller.friends = new Jog.Controller.Friends;
+          Jog.start = function() {
+            Jog.Controller.main = new Jog.Controller.Main;
+            Jog.Controller.session = new Jog.Controller.Session;
+            Jog.Controller.posts = new Jog.Controller.Posts;
+            Jog.Controller.friends = new Jog.Controller.Friends;
 
-      try {
-        Backbone.history.start();
-      }
-      catch (e) {
-        if (e.message != 'Backbone.history has already been started') {
-          throw e;
-        }
-      }
-      Jog.Controller.main.start();
-    };
+            try {
+              Backbone.history.start();
+            }
+            catch (e) {
+              if (e.message != 'Backbone.history has already been started') {
+                throw e;
+              }
+            }
+            Jog.Controller.main.start();
+          };
 
-    Jog.config = new Jog.Config({env: ENV});
+          Jog.config = new Jog.Config({env: ENV});
 
-    if (Jog.config.env == 'test') {
-      var specs = [
-        'order!cs!spec/helpers',
-        'order!cs!spec/integration/startup_spec',
-        'order!cs!spec/models/user_spec'
-      ];
-      require(
-        [
-          'text!vendor/jasmine-1.0.2/jasmine.css',
-          'order!vendor/jasmine-1.0.2/jasmine',
-          'order!vendor/jasmine-1.0.2/jasmine-html',
-          'order!vendor/sinon-1.0.0'
-        ].concat(specs), function(css) {
-          $('<style type="text/css">').text(css).appendTo('head');
+          if (Jog.config.env == 'test') {
+            var specs = [
+              'cs!spec/helpers',
+              'cs!spec/integration/startup_spec',
+              'cs!spec/models/user_spec'
+            ];
+            var spec_deps = [
+              'text!vendor/jasmine-1.0.2/jasmine.css',
+              'vendor/jasmine-1.0.2/jasmine',
+              'vendor/jasmine-1.0.2/jasmine-html',
+              'vendor/sinon-1.0.0'
+            ];
+            require(spec_deps, function(css) {
+                require(specs, function() {
+                    $('<style type="text/css">').text(css).appendTo('head');
 
-          ignite_with(function() {
-              jasmine.getEnv().addReporter(new jasmine.TrivialReporter());
-              jasmine.getEnv().execute();
-          });
-      })
-    }
-    else {
-      ignite_with(function() {
-          Jog.start();
+                    ignite_with(function() {
+                        jasmine.getEnv().addReporter(new jasmine.TrivialReporter());
+                        jasmine.getEnv().execute();
+                    });
+                });
+            });
+          }
+          else {
+            ignite_with(function() {
+                Jog.start();
+            });
+          }
       });
-    }
-  }
-);
+});
